73ce4970ed0ac3253b2b0ff43f079b7ace228862
danilo992 <tadeudanilo46@gmail.com>
2019-11-02 15:40:01 -0300

    tratando a primeira exceção

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
 
-PHPinfo();
+use ItemVenda\ItemVenda;
+use Venda\Venda;
 
-?>
+require_once "Venda.php";
+require_once "ItemVenda";
+
+try {
+    $camisa = new ItemVenda();
+    $camisa->setDescricao("Camisa polo");
+    $camisa->setPreco(11.0);
+
+    $bone = new ItemVenda();
+    $bone->setDescricao("boné Infatil");
+    $bone->setPreco(11.0);
+
+    $Venda = new Venda();
+    $Venda->adicionar(null);
+    $Venda->adicionar($bone);
+
+    print "Total da venda: "  . $Venda->getTotal();
+} catch (InvalidArgumentException $e) {
+    echo "Um erro aconteceu: " . $e->getMessage();
+} catch (LogicEception $e){
+    echo "Erro: " . $e->getMessage();
+}
